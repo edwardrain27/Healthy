b9cbe496e85905152730e3571bcb7d5e7f65792d
edwardrain27 <pablo522648@gmail.com>
2022-02-22 22:33:19 -0500

    primer commit

---
+++ src/app/pages/check/check.page.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-check',
+  templateUrl: './check.page.html',
+  styleUrls: ['./check.page.scss'],
+})
+export class CheckPage implements OnInit {
+
+  constructor() { }
+  data = [
+    {
+      name:'primary',
+      selected:false
+    },{
+      name:'secondary',
+      selected:true
+    },
+    {
+      name:'tertiary',
+      selected:false
+    },
+    {
+      name:'success',
+      selected:true
+    }
+  ]
+  ngOnInit() {
+  }
+
+  onClick( evento:any){
+    console.log(evento);
+  }
+  verData(){
+    console.log(this.data);
+  }
+}
